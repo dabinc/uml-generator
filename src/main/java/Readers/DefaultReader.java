a3fcfb793d72c8c5e304e594397102eee6b54ae6
Hussein Alawami <hussein.awami@kaust.edu.sa>
2018-12-16 16:47:53 -0500

    Fixed the problem of drawing multiple arrows between the same classes and interfaces

--- src/main/java/Readers/DefaultReader.java
+++ src/main/java/Readers/DefaultReader.java
@@ -1,8 +1,11 @@
 package Readers;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.ClassNode;
@@ -24,18 +27,29 @@ public class DefaultReader implements Reader {
 			System.out.println("DefaultReader could not find: " + classNames.get(i));
 			e.printStackTrace();
 		}
-		List<ClassNodeWrapper> toReturn = new ArrayList<ClassNodeWrapper>();
+		List<ClassNodeWrapper> toReturn = new LinkedList<ClassNodeWrapper>();
+		Set<String> passed = new HashSet<>();
 		for(ClassReader reader : classReaderList){
 			ClassNode classNode = new ClassNode();
 			reader.accept(classNode, ClassReader.EXPAND_FRAMES);
-			toReturn.add(new ClassNodeWrapper(classNode));
-			if(classNode.superName != null){
-				toReturn.add(new ClassNodeWrapper(classNode.superName, Optional.empty()));
+			ClassNodeWrapper toAdd = new ClassNodeWrapper(classNode);
+			toReturn.add(toAdd);
+			passed.add(toAdd.name);
+		}
+		Set<ClassNodeWrapper> neededClasses = new HashSet<>();
+		for(ClassNodeWrapper classNodeWrapper : toReturn){
+			if(classNodeWrapper.supername != null && !passed.contains(classNodeWrapper.supername)){
+				neededClasses.add(new ClassNodeWrapper(classNodeWrapper.supername, Optional.empty()));
+				passed.add(classNodeWrapper.supername);
 			}
-			for(String interfaceName : (List<String>)classNode.interfaces){
-				toReturn.add(new ClassNodeWrapper(interfaceName, Optional.of(Modifier.INTERFACE)));
+			for(String interfaceName : classNodeWrapper.interfaces){
+				if(!passed.contains(interfaceName)){
+					neededClasses.add(new ClassNodeWrapper(interfaceName, Optional.empty()));
+					passed.add(interfaceName);
+				}
 			}
 		}
+		toReturn.addAll(neededClasses);
 		return toReturn;
 	}
 
