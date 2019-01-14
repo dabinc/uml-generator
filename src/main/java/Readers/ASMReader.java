c153a8f9b0f73c449f639f261e5538505985e07f
Hussein Alawami <hussein.awami@kaust.edu.sa>
2019-01-14 08:57:08 -0500

    Fix array parsing for readers and made them ignore primitives (modify if there is a better way to check for primitives)

--- src/main/java/Readers/ASMReader.java
+++ src/main/java/Readers/ASMReader.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
 import Wrappers.ClassNodeWrapper;
 
@@ -16,12 +17,15 @@ public class ASMReader implements Reader {
 	public List<ClassNodeWrapper> getClassNodeWrappers(List<String> classNames) {
 		List<ClassReader> classReaderList = new ArrayList<ClassReader>();
 		for(int i = 0; i < classNames.size(); i++){
-			try{
-				classReaderList.add(new ClassReader(classNames.get(i)));
-			}
-			catch (IOException e) {
-				System.out.println("ASMReader could not find: " + classNames.get(i));
-				e.printStackTrace();
+			String name = removeArrayFromName(Type.getObjectType(classNames.get(i)).getClassName());
+			if(!isPrimitive(name)){
+				try{
+					classReaderList.add(new ClassReader(name));
+				}
+				catch (IOException e) {
+					System.out.println("ASMReader could not find: " + name);
+					e.printStackTrace();
+				}
 			}
 		}
 		
@@ -35,5 +39,21 @@ public class ASMReader implements Reader {
 			passed.add(toAdd.name);
 		}
 		return toReturn;
+	}
+	
+	public String removeArrayFromName(String name){
+		if(name.contains("[")){
+			return name.substring(0, name.indexOf('['));
+		}
+		return name;
+	}
+	
+	public boolean isPrimitive(String name){
+		if(name.equals(Type.BOOLEAN_TYPE.getClassName()) || name.equals(Type.BYTE_TYPE.getClassName()) || name.equals(Type.CHAR_TYPE.getClassName()) || name.equals(Type.DOUBLE_TYPE.getClassName())
+				|| name.equals(Type.FLOAT_TYPE.getClassName()) || name.equals(Type.INT_TYPE.getClassName()) || name.equals(Type.LONG_TYPE.getClassName()) || name.equals(Type.SHORT_TYPE.getClassName())
+						|| name.equals(Type.VOID_TYPE.getClassName())){
+			return true;
+		}
+		return false;
 	}
 }
