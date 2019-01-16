4e6b1c0836b2261ddeb5d08f481e03b819e49b5e
donohoc1 <donohoc1@rose-hulman.edu>
2019-01-15 21:44:53 -0500

    Cleaned up defaultprt. It introduced weird thing with double arrows.

--- src/main/java/PreRenderTasks/DefaultPreRenderTask.java
+++ src/main/java/PreRenderTasks/DefaultPreRenderTask.java
@@ -1,5 +1,6 @@
 package PreRenderTasks;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -58,77 +59,13 @@ public class DefaultPreRenderTask implements PreRenderTask{
 		for(ClassContainer classContainer : toReturn.classes){
 			visitedClasses.add(classContainer);
 			for(ClassContainer associatedClass : classContainer.associations){
-				boolean isOneToMany = false;
-				for(CardinalityWrapper cardinalityWrapper : classContainer.classNodeWrapper.associations){
-					if(cardinalityWrapper.toClass.equals(associatedClass.classNodeWrapper.name) && cardinalityWrapper.isOneToMany){
-						isOneToMany = true;
-						break;
-					}
-				}
-				ArrowContainer toAdd = isOneToMany ? new AssociationArrowContainer(associatedClass, classContainer, "*") : new AssociationArrowContainer(associatedClass, classContainer);	
-				if(associatedClass.associations.contains(classContainer) && !visitedClasses.contains(associatedClass)){
-					boolean isOtherOneToMany = false;
-					for(CardinalityWrapper cardinalityWrapper : associatedClass.classNodeWrapper.associations){
-						if(cardinalityWrapper.toClass.equals(classContainer.classNodeWrapper.name) && cardinalityWrapper.isOneToMany){
-							isOtherOneToMany = true;
-							break;
-						}
-					}
-					if(isOneToMany && isOtherOneToMany){
-						toAdd = new DoubleAssociationArrowContainer(classContainer, associatedClass, "*", "*");
-					}
-					else if(isOneToMany){
-						toAdd = new DoubleAssociationArrowContainer(classContainer, associatedClass, "*");
-					}
-					else if(isOtherOneToMany){
-						toAdd = new DoubleAssociationArrowContainer(classContainer, associatedClass, "1", "*");
-					}
-					else{
-						toAdd = new DoubleAssociationArrowContainer(classContainer, associatedClass);
-					}
-					toReturn.arrows.add(toAdd);
-				}
-				else if(!associatedClass.associations.contains(classContainer)){
-					toReturn.arrows.add(toAdd);
-				}				
+				toReturn.arrows.addAll(generateRelationshipArrows(classContainer, associatedClass, classContainer.classNodeWrapper.associations, associatedClass.classNodeWrapper.associations, visitedClasses, AssociationArrowContainer.class, DoubleAssociationArrowContainer.class));						
 			}
 			List<ClassContainer> nonDuplicateDependencies = new LinkedList<ClassContainer>();
 			nonDuplicateDependencies.addAll(classContainer.dependencies);
 			nonDuplicateDependencies.removeAll(classContainer.associations);
 			for(ClassContainer dependencyClass : nonDuplicateDependencies){
-				boolean isOneToMany = false;
-				for(CardinalityWrapper cardinalityWrapper : classContainer.classNodeWrapper.dependencies){
-					if(cardinalityWrapper.toClass.equals(dependencyClass.classNodeWrapper.name) && cardinalityWrapper.isOneToMany){
-						isOneToMany = true;
-						break;
-					}
-				}
-				ArrowContainer toAdd = isOneToMany ? new DependencyArrowContainer(dependencyClass, classContainer, "*") : new DependencyArrowContainer(dependencyClass, classContainer);
-				if(dependencyClass.dependencies.contains(classContainer) && !visitedClasses.contains(dependencyClass)){
-					boolean isOtherOneToMany = false;
-					for(CardinalityWrapper cardinalityWrapper : dependencyClass.classNodeWrapper.dependencies){
-						if(cardinalityWrapper.toClass.equals(classContainer.classNodeWrapper.name) && cardinalityWrapper.isOneToMany){
-							isOtherOneToMany = true;
-							break;
-						}
-					}
-					if(isOneToMany && isOtherOneToMany){
-						toAdd = new DoubleDependencyArrowContainer(classContainer, dependencyClass, "*", "*");
-					}
-					else if(isOneToMany){
-						toAdd = new DoubleDependencyArrowContainer(classContainer, dependencyClass, "*");
-					}
-					else if(isOtherOneToMany){
-						toAdd = new DoubleDependencyArrowContainer(classContainer, dependencyClass, "1", "*");
-					}
-					else{
-						toAdd = new DoubleDependencyArrowContainer(classContainer, dependencyClass);
-					}
-					toReturn.arrows.add(toAdd);
-				}
-				else if(!dependencyClass.dependencies.contains(classContainer)){
-					toReturn.arrows.add(toAdd);
-				}	
+				toReturn.arrows.addAll(generateRelationshipArrows(classContainer, dependencyClass, classContainer.classNodeWrapper.dependencies, dependencyClass.classNodeWrapper.dependencies, visitedClasses, DependencyArrowContainer.class, DoubleDependencyArrowContainer.class));
 			}
 			
 			if(classContainer.superclass.isPresent()){
@@ -141,5 +78,92 @@ public class DefaultPreRenderTask implements PreRenderTask{
 		
 		return toReturn;
 	}
+	
+	private <T extends ArrowContainer, K extends ArrowContainer> List<ArrowContainer> generateRelationshipArrows(ClassContainer fromClass, ClassContainer toClass, List<CardinalityWrapper> fromClassRelations, List<CardinalityWrapper> toClassRelations, List<ClassContainer> visitedClasses, Class<T> normalArrowClass, Class<K> doubleArrowClass){
+		List<ArrowContainer> toReturn = new LinkedList<ArrowContainer>();
+		boolean isOneToMany = false;
+		for(CardinalityWrapper cardinalityWrapper : fromClassRelations){
+			if(cardinalityWrapper.toClass.equals(toClass.classNodeWrapper.name) && cardinalityWrapper.isOneToMany){
+				isOneToMany = true;
+				break;
+			}
+		}
+		ArrowContainer toAdd = isOneToMany ? generateArrowContainer(normalArrowClass, toClass, fromClass, "*") : generateArrowContainer(normalArrowClass, toClass, fromClass);
+		if(toClassRelations.contains(fromClass) && !visitedClasses.contains(toClass)){
+			boolean isOtherOneToMany = false;
+			for(CardinalityWrapper cardinalityWrapper : toClassRelations){
+				if(cardinalityWrapper.toClass.equals(fromClass.classNodeWrapper.name) && cardinalityWrapper.isOneToMany){
+					isOtherOneToMany = true;
+					break;
+				}
+			}
+			if(isOneToMany && isOtherOneToMany){
+				toAdd = generateArrowContainer(doubleArrowClass, toClass, fromClass, "*", "*");
+			}
+			else if(isOneToMany){
+				toAdd = generateArrowContainer(doubleArrowClass, toClass, fromClass, "*");
+			}
+			else if(isOtherOneToMany){
+				toAdd = generateArrowContainer(doubleArrowClass, toClass, fromClass, "1", "*");
+			}
+			else{
+				toAdd = generateArrowContainer(doubleArrowClass, toClass, fromClass);
+			}
+			toReturn.add(toAdd);
+		}
+		else if(!toClassRelations.contains(fromClass)){
+			toReturn.add(toAdd);
+		}
+		return toReturn;
+	}
+	
+	private <T extends ArrowContainer> ArrowContainer generateArrowContainer(Class<T> arrowClass, Object... params){
+		Class<?>[] paramClasses = new Class<?>[params.length];
+		for(int i = 0; i < params.length; i++){
+			paramClasses[i] = params[i].getClass();
+		}
+		try {
+			return arrowClass.getConstructor(paramClasses).newInstance(params);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+			System.exit(-1);
+			return null;
+		}
+	}
+	
+//	boolean isOneToMany = false;
+//	for(CardinalityWrapper cardinalityWrapper : classContainer.classNodeWrapper.associations){
+//		if(cardinalityWrapper.toClass.equals(associatedClass.classNodeWrapper.name) && cardinalityWrapper.isOneToMany){
+//			isOneToMany = true;
+//			break;
+//		}
+//	}
+//	ArrowContainer toAdd = isOneToMany ? new AssociationArrowContainer(associatedClass, classContainer, "*") : new AssociationArrowContainer(associatedClass, classContainer);	
+//	if(associatedClass.associations.contains(classContainer) && !visitedClasses.contains(associatedClass)){
+//		boolean isOtherOneToMany = false;
+//		for(CardinalityWrapper cardinalityWrapper : associatedClass.classNodeWrapper.associations){
+//			if(cardinalityWrapper.toClass.equals(classContainer.classNodeWrapper.name) && cardinalityWrapper.isOneToMany){
+//				isOtherOneToMany = true;
+//				break;
+//			}
+//		}
+//		if(isOneToMany && isOtherOneToMany){
+//			toAdd = new DoubleAssociationArrowContainer(classContainer, associatedClass, "*", "*");
+//		}
+//		else if(isOneToMany){
+//			toAdd = new DoubleAssociationArrowContainer(classContainer, associatedClass, "*");
+//		}
+//		else if(isOtherOneToMany){
+//			toAdd = new DoubleAssociationArrowContainer(classContainer, associatedClass, "1", "*");
+//		}
+//		else{
+//			toAdd = new DoubleAssociationArrowContainer(classContainer, associatedClass);
+//		}
+//		toReturn.arrows.add(toAdd);
+//	}
+//	else if(!associatedClass.associations.contains(classContainer)){
+//		toReturn.arrows.add(toAdd);
+//	}	
 
 }
