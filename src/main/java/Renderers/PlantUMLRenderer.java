3180501d96bb729c1ecb59cdafe522ef02b7e156
donohoc1 <donohoc1@rose-hulman.edu>
2019-01-15 19:37:58 -0500

    Removed another artifact

--- src/main/java/Renderers/PlantUMLRenderer.java
+++ src/main/java/Renderers/PlantUMLRenderer.java
@@ -240,17 +240,5 @@ public class PlantUMLRenderer implements Renderer {
 		toReturn.append(renderDisplayContainerHashTag(doubleDependencyArrowContainer.displayContainer));
 		toReturn.append(System.lineSeparator());
 		return toReturn.toString();
-	}
-
-//	@Override
-//	public String modifyToTwoWay(String baseArrow) {
-//		String arrowAndAfter = baseArrow.substring(baseArrow.indexOf('<'));
-//		String arrow = arrowAndAfter.substring(0, arrowAndAfter.indexOf(' '));
-//		char arrowLineChar = arrow.charAt(arrow.length() - 1);
-//		String arrowHead = arrow.substring(1, arrow.indexOf(arrowLineChar));
-//		String newArrow = arrow + arrowHead + ">";
-//		String newFull = baseArrow.replace(arrow, newArrow);
-//		return newFull;
-//	}
-	
+	}	
 }
