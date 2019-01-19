444125568c6919863f6fa1a661252c0c943eeb33
donohoc1 <donohoc1@rose-hulman.edu>
2019-01-19 17:32:37 -0500

    Starting in on Renderer tests

---
+++ src/test/java/Renderers/PlantUMLRendererTest.java
@@ -0,0 +1,52 @@
+package Renderers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlantUMLRendererTest {
+	
+	private Renderer testRenderer;
+	
+	@Before
+	public void setup(){
+		testRenderer = new PlantUMLRenderer();
+	}
+
+	@Test
+	public void testRender() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenderDependencyArrowContainer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenderAssociationArrowContainer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenderInheritanceArrowContainer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenderImplementationArrowContainer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenderDoubleAssociationArrowContainer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenderDoubleDependencyArrowContainer() {
+		fail("Not yet implemented");
+	}
+
+}
