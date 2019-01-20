f7af53994e99d70071148592fd1e620029fd58e6
donohoc1 <donohoc1@rose-hulman.edu>
2019-01-20 13:34:31 -0500

    Hit a wall with mocking. Need to mock a list created in a method.

--- src/test/java/Readers/RecursiveReaderTest.java
+++ src/test/java/Readers/RecursiveReaderTest.java
@@ -40,39 +40,96 @@ public class RecursiveReaderTest {
 
 		EasyMock.replay(baseReader);
 
-		assertEquals(0, reader.getClassNodeWrappers(classesToTest).size());
+		List<ClassNodeWrapper> actual = reader.getClassNodeWrappers(classesToTest);
+
+		assertEquals(0, actual.size());
 	}
 
 	@Test
 	public void testRecurseToInheritance() {
+		String firstClass = "java.lang.String";
+		String secondClass = "java.lang.Object";
+
 		List<String> classesToTestFirstTime = new LinkedList<String>();
-		classesToTestFirstTime.add("java.lang.String");
-		
+		classesToTestFirstTime.add(firstClass);
+
 		List<String> classesToTestSecondTime = new LinkedList<String>();
-		classesToTestSecondTime.add("java.lang.Object");
-		
+		classesToTestSecondTime.add(secondClass);
+
 		List<ClassNodeWrapper> fromBaseReaderFirstTime = new LinkedList<ClassNodeWrapper>();
 		ClassNodeWrapper classNodeWrapperFirstTime = EasyMock.mock(ClassNodeWrapper.class);
-		classNodeWrapperFirstTime.supername = Optional.of("java.lang.Object");
+		classNodeWrapperFirstTime.name = firstClass;
+		classNodeWrapperFirstTime.supername = Optional.of(secondClass);
 		classNodeWrapperFirstTime.associations = new LinkedList<CardinalityWrapper>();
 		classNodeWrapperFirstTime.dependencies = new LinkedList<CardinalityWrapper>();
 		classNodeWrapperFirstTime.interfaces = new LinkedList<String>();
 		classNodeWrapperFirstTime.methodNodeWrappers = new LinkedList<MethodNodeWrapper>();
 		fromBaseReaderFirstTime.add(classNodeWrapperFirstTime);
-		
+
 		List<ClassNodeWrapper> fromBaseReaderSecondTime = new LinkedList<ClassNodeWrapper>();
-		
+		ClassNodeWrapper classNodeWrapperSecondTime = EasyMock.mock(ClassNodeWrapper.class);
+		classNodeWrapperSecondTime.name = secondClass;
+		classNodeWrapperSecondTime.supername = Optional.empty();
+		classNodeWrapperSecondTime.associations = new LinkedList<CardinalityWrapper>();
+		classNodeWrapperSecondTime.dependencies = new LinkedList<CardinalityWrapper>();
+		classNodeWrapperSecondTime.interfaces = new LinkedList<String>();
+		classNodeWrapperSecondTime.methodNodeWrappers = new LinkedList<MethodNodeWrapper>();
+		fromBaseReaderSecondTime.add(classNodeWrapperSecondTime);
+
 		EasyMock.expect(baseReader.getClassNodeWrappers(classesToTestFirstTime)).andReturn(fromBaseReaderFirstTime);
 		EasyMock.expect(baseReader.getClassNodeWrappers(classesToTestSecondTime)).andReturn(fromBaseReaderSecondTime);
-		
-		EasyMock.replay(classNodeWrapperFirstTime, baseReader);
-		
-		assertEquals(2, reader.getClassNodeWrappers(classesToTestFirstTime).size());
+
+		EasyMock.replay(classNodeWrapperFirstTime, classNodeWrapperSecondTime, baseReader);
+
+		List<ClassNodeWrapper> actual = reader.getClassNodeWrappers(classesToTestFirstTime);
+
+		assertEquals(2, actual.size());
+
+		EasyMock.verify(classNodeWrapperFirstTime, classNodeWrapperSecondTime);
 	}
 
 	@Test
 	public void testRecurseToImplementation() {
+		String firstClass = "java.lang.String";
+		String secondClass = "java.lang.Object";
 
+		List<String> classesToTestFirstTime = new LinkedList<String>();
+		classesToTestFirstTime.add(firstClass);
+
+		List<String> classesToTestSecondTime = new LinkedList<String>();
+		classesToTestSecondTime.add(secondClass);
+
+		List<ClassNodeWrapper> fromBaseReaderFirstTime = new LinkedList<ClassNodeWrapper>();
+		ClassNodeWrapper classNodeWrapperFirstTime = EasyMock.mock(ClassNodeWrapper.class);
+		classNodeWrapperFirstTime.name = firstClass;
+		classNodeWrapperFirstTime.supername = Optional.empty();
+		classNodeWrapperFirstTime.associations = new LinkedList<CardinalityWrapper>();
+		classNodeWrapperFirstTime.dependencies = new LinkedList<CardinalityWrapper>();
+		classNodeWrapperFirstTime.interfaces = new LinkedList<String>();
+		classNodeWrapperFirstTime.interfaces.add(secondClass);
+		classNodeWrapperFirstTime.methodNodeWrappers = new LinkedList<MethodNodeWrapper>();
+		fromBaseReaderFirstTime.add(classNodeWrapperFirstTime);
+
+		List<ClassNodeWrapper> fromBaseReaderSecondTime = new LinkedList<ClassNodeWrapper>();
+		ClassNodeWrapper classNodeWrapperSecondTime = EasyMock.mock(ClassNodeWrapper.class);
+		classNodeWrapperSecondTime.name = secondClass;
+		classNodeWrapperSecondTime.supername = Optional.empty();
+		classNodeWrapperSecondTime.associations = new LinkedList<CardinalityWrapper>();
+		classNodeWrapperSecondTime.dependencies = new LinkedList<CardinalityWrapper>();
+		classNodeWrapperSecondTime.interfaces = new LinkedList<String>();
+		classNodeWrapperSecondTime.methodNodeWrappers = new LinkedList<MethodNodeWrapper>();
+		fromBaseReaderSecondTime.add(classNodeWrapperSecondTime);
+
+		EasyMock.expect(baseReader.getClassNodeWrappers(classesToTestFirstTime)).andReturn(fromBaseReaderFirstTime);
+		EasyMock.expect(baseReader.getClassNodeWrappers(classesToTestSecondTime)).andReturn(fromBaseReaderSecondTime);
+
+		EasyMock.replay(classNodeWrapperFirstTime, baseReader);
+
+		List<ClassNodeWrapper> actual = reader.getClassNodeWrappers(classesToTestFirstTime);
+
+		assertEquals(2, actual.size());
+
+		EasyMock.verify(classNodeWrapperFirstTime, classNodeWrapperSecondTime);
 	}
 
 	@Test
