b9322f048c7feeb3bbb46e41cfff4b08e14053ae
donohoc1 <donohoc1@rose-hulman.edu>
2019-01-21 22:31:20 -0500

    A little more work on the singleton.

--- src/main/java/PreRenderTasks/SingletonPatternDetectorPreRenderTask.java
+++ src/main/java/PreRenderTasks/SingletonPatternDetectorPreRenderTask.java
@@ -3,40 +3,67 @@ package PreRenderTasks;
 import java.util.Optional;
 
 import Containers.ClassContainer;
+import Containers.FieldContainer;
+import Containers.MethodContainer;
 import Containers.ProgramContainer;
 import Containers.StereotypeContainer;
+import Enums.Modifier;
 
 public class SingletonPatternDetectorPreRenderTask extends PreRenderTaskDecorator {
 
 	public SingletonPatternDetectorPreRenderTask(PreRenderTask preRenderTask) {
 		super(preRenderTask);
 	}
-	
+
 	@Override
-	public ProgramContainer getProgramContainer(){
+	public ProgramContainer getProgramContainer() {
 		ProgramContainer toReturn = super.getProgramContainer();
-		for(ClassContainer classContainer : toReturn.classes){
-			if(isSingleton(classContainer)){
+		for (ClassContainer classContainer : toReturn.classes) {
+			if (isSingleton(classContainer)) {
 				classContainer.displayContainer.color = Optional.of("blue");
-				if(classContainer.stereotypeContainer.isPresent()){
-					if(classContainer.stereotypeContainer.get().label.isPresent()){
-						classContainer.stereotypeContainer.get().label = Optional.of(classContainer.stereotypeContainer.get().label.get() + "," + "Singleton");
-					}
-					else{
+				if (classContainer.stereotypeContainer.isPresent()) {
+					if (classContainer.stereotypeContainer.get().label.isPresent()) {
+						classContainer.stereotypeContainer.get().label = Optional
+								.of(classContainer.stereotypeContainer.get().label.get() + "," + "Singleton");
+					} else {
 						classContainer.stereotypeContainer.get().label = Optional.of("Singleton");
 					}
-					
-				}
-				else{
+
+				} else {
 					classContainer.stereotypeContainer = Optional.of(new StereotypeContainer("Singleton"));
 				}
 			}
 		}
 		return toReturn;
 	}
-	
-	private boolean isSingleton(ClassContainer classContainer){
-		//TODO
+
+	private boolean isSingleton(ClassContainer classContainer) {
+		return hasGetInstanceMethod(classContainer) && hasStaticFieldOfSelf(classContainer)
+				&& hasPrivateConstructor(classContainer);
+	}
+
+	private boolean hasStaticFieldOfSelf(ClassContainer classContainer) {
+		for (FieldContainer fieldContainer : classContainer.fields) {
+			if (fieldContainer.fieldNodeWrapper.modifiers.contains(Modifier.STATIC)
+					&& fieldContainer.fieldNodeWrapper.type.equals(classContainer.classNodeWrapper.name)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean hasPrivateConstructor(ClassContainer classContainer) {
+		// TODO
+		return false;
+	}
+
+	private boolean hasGetInstanceMethod(ClassContainer classContainer) {
+		for (MethodContainer methodContainer : classContainer.methods) {
+			if (methodContainer.methodNodeWrapper.modifiers.contains(Modifier.STATIC)
+					&& methodContainer.methodNodeWrapper.type.equals(classContainer.classNodeWrapper.name)) {
+				return true;
+			}
+		}
 		return false;
 	}
 
