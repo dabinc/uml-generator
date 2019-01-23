8afbdd8487cd680b46ee1a58a10c1ba0d172c0c0
donohoc1 <donohoc1@rose-hulman.edu>
2019-01-23 11:45:12 -0500

    Refactored ArrowContainer to not be dumb anymore

--- src/main/java/Containers/ArrowContainer.java
+++ src/main/java/Containers/ArrowContainer.java
@@ -1,7 +1,23 @@
 package Containers;
 
+import java.util.Optional;
+
 import Renderers.Renderer;
 
-public interface ArrowContainer {
-	public String render(Renderer renderer);
+public abstract class ArrowContainer{
+	public ClassContainer to;
+	public ClassContainer from;
+	public Optional<String> toCardinality;
+	public Optional<String> fromCardinality;
+	public DisplayContainer displayContainer;
+	
+	public ArrowContainer(ClassContainer to, ClassContainer from, String toCardinality, String fromCardinality){
+		this.to = to;
+		this.from = from;
+		this.toCardinality = Optional.ofNullable(toCardinality);
+		this.fromCardinality = Optional.ofNullable(fromCardinality);
+		this.displayContainer = new DisplayContainer();
+	}
+	
+	public abstract String render(Renderer renderer);
 }
--- src/main/java/PreRenderTasks/InheritanceOverCompositionDetectorPreRenderTask.java
+++ src/main/java/PreRenderTasks/InheritanceOverCompositionDetectorPreRenderTask.java
@@ -1,10 +1,28 @@
 package PreRenderTasks;
 
+import Containers.ArrowContainer;
+import Containers.ClassContainer;
+import Containers.InheritanceArrowContainer;
+import Containers.ProgramContainer;
+
 public class InheritanceOverCompositionDetectorPreRenderTask extends PreRenderTaskDecorator {
 
 	public InheritanceOverCompositionDetectorPreRenderTask(PreRenderTask preRenderTask) {
 		super(preRenderTask);
-		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public ProgramContainer getProgramContainer() {
+		ProgramContainer programContainer = super.getProgramContainer();
+		
+		for(ArrowContainer arrowContainer : programContainer.arrows){
+			if(InheritanceArrowContainer.class.isAssignableFrom(arrowContainer.getClass())){
+				ClassContainer to = arrowContainer.to;
+				
+			}
+		}
+		
+		return programContainer;
 	}
 
 }
