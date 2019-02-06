e3dba999a3fdd66d03c86ff7f9f5bb14e8470cd5
donohoc1 <donohoc1@rose-hulman.edu>
2019-02-05 21:10:23 -0500

    Added some info to InstructionNodeWrapper

--- src/main/java/Wrappers/InstructionNodeWrapper.java
+++ src/main/java/Wrappers/InstructionNodeWrapper.java
@@ -1,10 +1,25 @@
 package Wrappers;
 
+import java.util.Optional;
+
+import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
 
 public class InstructionNodeWrapper {
+	
+	public Optional<String> methodName;
+	public Optional<String> methodOwner;
 
 	public InstructionNodeWrapper(AbstractInsnNode abstractInsnNode) {
-		// TODO Auto-generated constructor stub
+		this.methodName = Optional.empty();
+		this.methodOwner = Optional.empty();
+		if(abstractInsnNode instanceof MethodInsnNode){
+			MethodInsnNode methodInsnNode = (MethodInsnNode) abstractInsnNode;
+			this.methodName = Optional.ofNullable(methodInsnNode.name);
+			if(methodInsnNode.owner != null){
+				this.methodOwner = Optional.ofNullable(Type.getObjectType(methodInsnNode.owner).getClassName());
+			}			
+		}
 	}
 }
