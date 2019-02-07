24f8ed9e60407b6a2da7af5cf02119cfae78d0a1
donohoc1 <donohoc1@rose-hulman.edu>
2019-02-06 23:40:41 -0500

    Removed comments and cleaned up

--- src/main/java/PreRenderTasks/BadDecoratorPatternDetectorPreRenderTask.java
+++ src/main/java/PreRenderTasks/BadDecoratorPatternDetectorPreRenderTask.java
@@ -1,25 +1,7 @@
 package PreRenderTasks;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import Containers.ArrowContainer;
-import Containers.AssociationArrowContainer;
-import Containers.ClassContainer;
-import Containers.FakeMethodContainer;
-import Containers.ImplementationArrowContainer;
-import Containers.InheritanceArrowContainer;
-import Containers.MethodContainer;
 import Containers.ProgramContainer;
-import Containers.StereotypeContainer;
-import Enums.Modifier;
 import Utilities.DecoratorPatternRunner;
-import Wrappers.MethodNodeWrapper;
-import Wrappers.ParameterNodeWrapper;
 
 public class BadDecoratorPatternDetectorPreRenderTask extends PreRenderTaskDecorator {
 
@@ -29,105 +11,8 @@ public class BadDecoratorPatternDetectorPreRenderTask extends PreRenderTaskDecor
 
 	@Override
 	public ProgramContainer getProgramContainer() {
-		ProgramContainer toReturn = super.getProgramContainer();
 		DecoratorPatternRunner decoratorPatternRunner = new DecoratorPatternRunner();
-
-//		for (ArrowContainer inheritanceOrImplementation : toReturn.arrows) {
-//			if (inheritanceOrImplementation instanceof InheritanceArrowContainer
-//					|| inheritanceOrImplementation instanceof ImplementationArrowContainer) {
-//				for (ArrowContainer association : toReturn.arrows) {
-//					if (association instanceof AssociationArrowContainer
-//							&& association.from.equals(inheritanceOrImplementation.from)
-//							&& association.to.equals(inheritanceOrImplementation.to)) {
-//						ClassContainer decoratedClass = inheritanceOrImplementation.to;
-//						ClassContainer abstractDecorator = inheritanceOrImplementation.from;
-//						Collection<MethodContainer> nonOverridenMethods = overridesAllConcreteMethods(
-//								inheritanceOrImplementation);
-//						if (nonOverridenMethods.size() != 0) {
-//							decoratedClass.stereotypeContainer.add(new StereotypeContainer("component"));
-//							association.stereotypeContainer.add(new StereotypeContainer("decorates"));
-//							decoratedClass.displayContainer.color = Optional.of("lightgreen");
-//							abstractDecorator.displayContainer.color = Optional.of("lightgreen");
-//							abstractDecorator.stereotypeContainer.add(new StereotypeContainer("Bad decorator"));
-//							for (ArrowContainer inheritanceOrImplementationToAbstractDecorator : toReturn.arrows) {
-//								if ((inheritanceOrImplementationToAbstractDecorator instanceof InheritanceArrowContainer
-//										|| inheritanceOrImplementationToAbstractDecorator instanceof ImplementationArrowContainer)
-//										&& inheritanceOrImplementationToAbstractDecorator.to
-//												.equals(abstractDecorator)) {
-//									ClassContainer concreteDecorator = inheritanceOrImplementationToAbstractDecorator.from;
-//									concreteDecorator.displayContainer.color = Optional.of("lightgreen");
-//									concreteDecorator.stereotypeContainer.add(new StereotypeContainer("decorator"));
-//								}
-//							}
-//							for (MethodContainer nonOverridenMethod : nonOverridenMethods) {
-//								if (nonOverridenMethod.methodNodeWrapper.isPresent()) {
-//									MethodNodeWrapper methodNodeWrapper = nonOverridenMethod.methodNodeWrapper.get();
-//									String name = methodNodeWrapper.name;
-//									List<String> parameters = new LinkedList<String>();
-//									for (ParameterNodeWrapper parameterNodeWrapper : methodNodeWrapper.parameterNodeWrappers) {
-//										parameters.add(parameterNodeWrapper.type);
-//									}
-//									List<Modifier> modifiers = methodNodeWrapper.modifiers;
-//									String returnType = methodNodeWrapper.type;
-//									MethodContainer toAdd = new FakeMethodContainer(name, parameters, modifiers,
-//											returnType);
-//									toAdd.displayContainer.color = Optional.of("red");
-//									abstractDecorator.methods.add(toAdd);
-//								}
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
-		return decoratorPatternRunner.runDecoratorPatterDetection(toReturn, true);
+		return decoratorPatternRunner.runDecoratorPatterDetection(super.getProgramContainer(), true);
 	}
-
-//	private Collection<MethodContainer> overridesAllConcreteMethods(ArrowContainer arrowContainer) {
-//		List<MethodContainer> toMethodContainers = arrowContainer.to.methods;
-//		List<MethodContainer> fromMethodContainers = arrowContainer.from.methods;
-//		List<MethodNodeWrapper> toMethodNodeWrappers = new LinkedList<MethodNodeWrapper>();
-//		List<MethodNodeWrapper> fromMethodNodeWrappers = new LinkedList<MethodNodeWrapper>();
-//		Map<String, MethodContainer> nonOverriddenMethods = new HashMap<String, MethodContainer>();
-//		for (MethodContainer toMethodContainer : toMethodContainers) {
-//			if (toMethodContainer.methodNodeWrapper.isPresent()) {
-//				MethodNodeWrapper toMethodNodeWrapper = toMethodContainer.methodNodeWrapper.get();
-//				toMethodNodeWrappers.add(toMethodNodeWrapper);
-//				if (!toMethodNodeWrapper.name.equals("<init>")
-//						&& !toMethodNodeWrapper.modifiers.contains(Modifier.ABSTRACT)) {
-//					nonOverriddenMethods.put(toMethodNodeWrapper.name, toMethodContainer);
-//				}
-//			}
-//		}
-//		for (MethodContainer fromMethodContainer : fromMethodContainers) {
-//			if (fromMethodContainer.methodNodeWrapper.isPresent()) {
-//				fromMethodNodeWrappers.add(fromMethodContainer.methodNodeWrapper.get());
-//			}
-//		}
-//		for (MethodNodeWrapper toMethodNodeWrapper : toMethodNodeWrappers) {
-//			if (!toMethodNodeWrapper.modifiers.contains(Modifier.ABSTRACT)) {
-//				for (MethodNodeWrapper fromMethodNodeWrapper : fromMethodNodeWrappers) {
-//					if (!fromMethodNodeWrapper.modifiers.contains(Modifier.ABSTRACT)) {
-//						if (fromMethodNodeWrapper.name.equals(toMethodNodeWrapper.name)
-//								&& !fromMethodNodeWrapper.name.equals("<init>")
-//								&& fromMethodNodeWrapper.parameterNodeWrappers
-//										.size() == toMethodNodeWrapper.parameterNodeWrappers.size()) {
-//							int i;
-//							for (i = 0; i < fromMethodNodeWrapper.parameterNodeWrappers.size(); i++) {
-//								if (!fromMethodNodeWrapper.parameterNodeWrappers.get(i).type
-//										.equals(toMethodNodeWrapper.parameterNodeWrappers.get(i).type)) {
-//									break;
-//								}
-//							}
-//							if (i == fromMethodNodeWrapper.parameterNodeWrappers.size()) {
-//								nonOverriddenMethods.remove(fromMethodNodeWrapper.name);
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
-//		return nonOverriddenMethods.values();
-//	}
 
 }
--- src/main/java/PreRenderTasks/DecoratorPatternDetectorPreRenderTask.java
+++ src/main/java/PreRenderTasks/DecoratorPatternDetectorPreRenderTask.java
@@ -1,19 +1,7 @@
 package PreRenderTasks;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
-
-import Containers.ArrowContainer;
-import Containers.AssociationArrowContainer;
-import Containers.ClassContainer;
-import Containers.ImplementationArrowContainer;
-import Containers.InheritanceArrowContainer;
 import Containers.ProgramContainer;
-import Containers.StereotypeContainer;
-import Enums.Modifier;
 import Utilities.DecoratorPatternRunner;
-import Wrappers.MethodNodeWrapper;
 
 public class DecoratorPatternDetectorPreRenderTask extends PreRenderTaskDecorator {
 
@@ -23,74 +11,8 @@ public class DecoratorPatternDetectorPreRenderTask extends PreRenderTaskDecorato
 
 	@Override
 	public ProgramContainer getProgramContainer() {
-		ProgramContainer toReturn = super.getProgramContainer();
 		DecoratorPatternRunner decoratorPatternRunner = new DecoratorPatternRunner();
-
-//		for (ArrowContainer inheritanceOrImplementation : toReturn.arrows) {
-//			if (inheritanceOrImplementation instanceof InheritanceArrowContainer
-//					|| inheritanceOrImplementation instanceof ImplementationArrowContainer) {
-//				for (ArrowContainer association : toReturn.arrows) {
-//					if (association instanceof AssociationArrowContainer
-//							&& association.from.equals(inheritanceOrImplementation.from)
-//							&& association.to.equals(inheritanceOrImplementation.to)) {
-//						ClassContainer decoratedClass = inheritanceOrImplementation.to;
-//						ClassContainer abstractDecorator = inheritanceOrImplementation.from;
-//						if(overridesAllConcreteMethods(inheritanceOrImplementation)){
-//							decoratedClass.stereotypeContainer.add(new StereotypeContainer("component"));
-//							association.stereotypeContainer.add(new StereotypeContainer("decorates"));
-//							decoratedClass.displayContainer.color = Optional.of("lightgreen");
-//							abstractDecorator.displayContainer.color = Optional.of("lightgreen");
-//							abstractDecorator.stereotypeContainer.add(new StereotypeContainer("decorator"));
-//							for(ArrowContainer inheritanceOrImplementationToAbstractDecorator : toReturn.arrows){
-//								if((inheritanceOrImplementationToAbstractDecorator instanceof InheritanceArrowContainer ||
-//										inheritanceOrImplementationToAbstractDecorator instanceof ImplementationArrowContainer) &&
-//										inheritanceOrImplementationToAbstractDecorator.to.equals(abstractDecorator)){
-//									ClassContainer concreteDecorator = inheritanceOrImplementationToAbstractDecorator.from;
-//									concreteDecorator.displayContainer.color = Optional.of("lightgreen");
-//									concreteDecorator.stereotypeContainer.add(new StereotypeContainer("decorator"));
-//								}
-//							}
-//						}	
-//					}
-//				}
-//			}
-//		}
-		return decoratorPatternRunner.runDecoratorPatterDetection(toReturn, false);
+		return decoratorPatternRunner.runDecoratorPatterDetection(super.getProgramContainer(), false);
 	}
-
-//	private boolean overridesAllConcreteMethods(ArrowContainer arrowContainer) {
-//		List<MethodNodeWrapper> toMethodNodeWrappers = arrowContainer.to.classNodeWrapper.methodNodeWrappers;
-//		List<MethodNodeWrapper> fromMethodNodeWrappers = arrowContainer.from.classNodeWrapper.methodNodeWrappers;
-//		List<String> fromMethods = new LinkedList<>();
-//		for(MethodNodeWrapper toMethodNodeWrapper : toMethodNodeWrappers){
-//			if(!toMethodNodeWrapper.name.equals("<init>") && !toMethodNodeWrapper.modifiers.contains(Modifier.ABSTRACT)){
-//				fromMethods.add(toMethodNodeWrapper.name);
-//			}
-//		}
-//		for (MethodNodeWrapper toMethodNodeWrapper : toMethodNodeWrappers) {
-//			if (!toMethodNodeWrapper.modifiers.contains(Modifier.ABSTRACT)) {
-//				for (MethodNodeWrapper fromMethodNodeWrapper : fromMethodNodeWrappers) {
-//					if (!fromMethodNodeWrapper.modifiers.contains(Modifier.ABSTRACT)) {
-//						if (fromMethodNodeWrapper.name.equals(toMethodNodeWrapper.name)
-//								&& !fromMethodNodeWrapper.name.equals("<init>")
-//								&& fromMethodNodeWrapper.parameterNodeWrappers
-//										.size() == toMethodNodeWrapper.parameterNodeWrappers.size()) {
-//							int i;
-//							for (i = 0; i < fromMethodNodeWrapper.parameterNodeWrappers.size(); i++) {
-//								if (!fromMethodNodeWrapper.parameterNodeWrappers.get(i).type
-//										.equals(toMethodNodeWrapper.parameterNodeWrappers.get(i).type)) {	
-//									break;
-//								}
-//							}
-//							if(i == fromMethodNodeWrapper.parameterNodeWrappers.size()){
-//								fromMethods.remove(fromMethodNodeWrapper.name);
-//							}
-//						}
-//					}
-//				}
-//			}
-//		} 
-//		return fromMethods.size() == 0;
-//	} 
 
 }
