a459539b4ce678ebc7c917d9dc1060117a41edae
donohoc1 <donohoc1@rose-hulman.edu>
2019-04-03 19:27:27 -0400

    Initial working state.
    Still need to add in method desc to differentiate overloaded methods.
    Still need to write tests. Still need to go through and clean up the
    changed code cuz it ugly. Still need to figure out if there is a better
    way to have an empty abstraction for all insnnodes.

--- src/main/java/Wrappers/MethodInstructionNodeWrapper.java
+++ src/main/java/Wrappers/MethodInstructionNodeWrapper.java
@@ -2,7 +2,10 @@ package Wrappers;
 
 import java.util.Optional;
 
-public class MethodInstructionNodeWrapper extends InstructionNodeWrapper {
+public class MethodInstructionNodeWrapper implements InstructionNodeWrapper {
+	
+	public Optional<String> methodName;
+	public Optional<String> methodOwner;
 
 	public MethodInstructionNodeWrapper(String methodName, String methodOwner) {
 		this.methodName = Optional.ofNullable(methodName);
