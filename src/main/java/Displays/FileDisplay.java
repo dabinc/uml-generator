c8aece652db6ad6e1369da2187911d764fd618bb
choid <choid@choid-5520.rose-hulman.edu>
2019-05-08 13:37:50 -0400

    having user decide file name

--- src/main/java/Displays/FileDisplay.java
+++ src/main/java/Displays/FileDisplay.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
 
 public class FileDisplay implements Display {
 
@@ -11,19 +12,22 @@ public class FileDisplay implements Display {
 	public void display(String umlCode) {
 		PrintWriter writer;
 		File tempDirectory = new File("temp");
-		if(!tempDirectory.exists()){
+		if (!tempDirectory.exists()) {
 			tempDirectory.mkdir();
 		}
 		try {
-			writer = new PrintWriter("temp" + System.getProperty("file.separator") + "UMLGenerator-"
-					+ System.currentTimeMillis() + ".txt", "UTF-8");
+			Scanner in = new Scanner(System.in);
+			System.out.println("What is the filename?\n");
+			String input = in.nextLine();
+			File file = new File(input);
+			writer = new PrintWriter("temp" + System.getProperty("file.separator") + file + ".txt", "UTF-8");
 			writer.print(umlCode);
 			writer.close();
+			System.out.println("Successfully generated txt file.");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
